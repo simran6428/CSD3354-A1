f67be23e97e5f1a1854d521071a98abdca8e5dca
visualstudio <visualstudio@122-PC28>
2019-02-20 12:12:04 -0500

    v

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,8 +8,89 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // new yogesh().veerpal();
+            Console.WriteLine(UsingRecursion.CalculateRecursively(2, 8));
+
+
+        }
+
+    }
+    class yogesh
+    {
+        static int j = 0;
+        public void veerpal()
+        { // let's write a Trace message
+            Console.WriteLine(yogesh.j++);
+            int Sum = 0;
+            Sum += 1;
+            veerpal();
         }
     }
+
+    class UsingRecursion
+    {
+        // Create an application which 
+        // calculates the sum of all the numbers from n to m recursively:
+
+        public static int CalculateRecursively(int n, int m)
+        {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateRecursively(n, m);
+            }
+            return sum;
+        }
+
+
+    }
+
+    class CountrySide
+    {
+        // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+        Village Maeland;
+        Village Helmholtz;
+        Village Alst;
+        Village Wessig;
+        Village Badden;
+        Village Uster;
+        Village Schvenig;
+
+
+    }
+    public void Run()
+    
+        {
+           
+            Village Alst = new Village("Alst", false);
+            Village Schvenig = new Village("Schvenig", false);
+            Village Wessig = new Village("Wessig", true);
+            Alst.west = Schvenig;
+            Alst.east = Wessig;
+            Alst.distanceToWestVillage = 14;
+            Alst.distanceToEastVillage = 19;
+        }
+
+
+        class Village
+        {
+            public Village(string _villageName, bool _isAHere)
+            {
+                isAstrildgeHere = _isAHere;
+                VillageName = _villageName;
+            }
+
+            public Village west;
+            public Village east;
+            public string VillageName;
+            public int distanceToNextVillage;
+            public int distanceToPreviousVillage;
+            public bool isAstrildgeHere;
+        }
+
+    
 }
